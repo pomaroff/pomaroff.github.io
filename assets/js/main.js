933d2a08ba4fac6bee2855258c48195e4261d469
pomaroff <65222259+pomaroff@users.noreply.github.com>
2022-01-23 20:42:01 +1100

    retry

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,24 @@
 $('body').addClass('is-loading');
 
-function loaded() {
+function on_load() {
 	window.setTimeout(function() {
 		$('body').removeClass('is-loading');
 	}, 100);
 }
 
+function on_resize() {
+	document.querySelector(':root').style.setProperty('--vh', window.innerHeight/100 + 'px');
+}
+
 window.addEventListener
-	? window.addEventListener("load", loaded, false)
-	: window.attachEvent && window.attachEvent("onload", loaded);
+	? window.addEventListener("load", on_load, false)
+	: window.attachEvent && window.attachEvent("onload", on_load);
+
+window.addEventListener
+	? window.addEventListener("resize", on_resize, false)
+	: window.attachEvent && window.attachEvent("resize", on_resize);
+
+on_resize();
 
 (function($) {
 
@@ -24,15 +34,12 @@ window.addEventListener
 	$(function() {
 
 		var	$window = $(window),
-			$html = $('html'),
 			$body = $('body'),
-			$wrapper = $('#page-wrapper'),
 			$banner = $('#banner'),
 			$header = $('#header');
 
 		// Mobile?
 			if (skel.vars.mobile) {
-				$html.addClass('is-mobile');
 				$body.addClass('is-mobile');
 			} else {
 				skel
@@ -84,7 +91,9 @@ window.addEventListener
 			if ($banner.length > 0
 			&&	$header.hasClass('alt')) {
 
-				$window.on('resize', function() { $window.trigger('scroll'); });
+				window.addEventListener('resize', () => { 
+					$window.trigger('scroll');
+				});
 
 				$banner.scrollex({
 					bottom:		$header.outerHeight() + 1,
